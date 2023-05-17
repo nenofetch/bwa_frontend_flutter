95746b5a19668838979db92b44d78c6f8a7322d8
Neno Arisma <nenoarisma@gmail.com>
2023-05-18 00:42:41 +0700

    chore: update signin page

--- lib/themes.dart
+++ lib/themes.dart
@@ -12,6 +12,7 @@ Color backgroundColor2 = const Color(0xff2B2937);
 Color backgroundColor3 = const Color(0xff242231);
 Color primaryTextColor = const Color(0xffF1F0F2);
 Color secondaryTextColor = const Color(0xff999999);
+Color subtitleColor = const Color(0xff504F5E);
 
 TextStyle primaryTextStyle = GoogleFonts.poppins(
   color: primaryTextColor,
@@ -23,6 +24,14 @@ TextStyle secondaryTextStyle = GoogleFonts.poppins(
 
 TextStyle priceTextStyle = GoogleFonts.poppins(
   color: priceColor,
+);
+
+TextStyle subtitleTextStyle = GoogleFonts.poppins(
+  color: subtitleColor,
+);
+
+TextStyle purpleColor = GoogleFonts.poppins(
+  color: primaryColor,
 );
 
 FontWeight light = FontWeight.w300;
