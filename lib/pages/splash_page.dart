591314cf60651337c44a682b84104df7b12f464f
Neno Arisma <nenoarisma@gmail.com>
2023-05-17 23:59:17 +0700

    feat: adding splash screen page

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:shamo_frontend/themes.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    Timer(Duration(seconds: 3), () => Navigator.pushNamed(context, '/sign-in'));
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor1,
+      body: Center(
+        child: Container(
+          width: 130,
+          height: 150,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                'assets/image_splash.png',
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
