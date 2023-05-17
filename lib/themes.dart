dbf2230b92ae79cb6f47375cc3f6ce735e91b138
Neno Arisma <nenoarisma@gmail.com>
2023-05-17 23:58:58 +0700

    chore: adding themes

--- lib/themes.dart
+++ lib/themes.dart
@@ -1,15 +1,17 @@
 import "package:flutter/material.dart";
 import 'package:google_fonts/google_fonts.dart';
 
-Color primaryColor = Color(0xff6C5ECF);
-Color secondaryColor = Color(0xff38ABBE);
-Color alertColor = Color(0xffED6363);
-Color priceColor = Color(0xff2C96F1);
-Color backgroundColor1 = Color(0xff1F1D2B);
-Color backgroundColor2 = Color(0xff2B2937);
-Color backgroundColor3 = Color(0xff242231);
-Color primaryTextColor = Color(0xffF1F0F2);
-Color secondaryTextColor = Color(0xff999999);
+double defaultMargin = 30.0;
+
+Color primaryColor = const Color(0xff6C5ECF);
+Color secondaryColor = const Color(0xff38ABBE);
+Color alertColor = const Color(0xffED6363);
+Color priceColor = const Color(0xff2C96F1);
+Color backgroundColor1 = const Color(0xff1F1D2B);
+Color backgroundColor2 = const Color(0xff2B2937);
+Color backgroundColor3 = const Color(0xff242231);
+Color primaryTextColor = const Color(0xffF1F0F2);
+Color secondaryTextColor = const Color(0xff999999);
 
 TextStyle primaryTextStyle = GoogleFonts.poppins(
   color: primaryTextColor,
