34b785ed193dfe0ece1d8c4972153e7ec2169f75
Neno Arisma <nenoarisma@gmail.com>
2023-05-20 22:32:13 +0700

    feat: adding route for detail chat

---
+++ lib/pages/home/detail_chat_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:shamo_frontend/themes.dart';
+
+class DetailChatPage extends StatelessWidget {
+  const DetailChatPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    PreferredSizeWidget header() {
+      return PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: AppBar(
+          backgroundColor: backgroundColor1,
+          centerTitle: false,
+          title: Row(
+            children: [
+              Image.asset(
+                'assets/image_shop_logo_online.png',
+                width: 50,
+              ),
+              SizedBox(
+                width: 12,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Shoe Store',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 14,
+                      fontWeight: medium,
+                    ),
+                  ),
+                  Text(
+                    'Online',
+                    style: secondaryTextStyle.copyWith(
+                      fontSize: 14,
+                      fontWeight: light,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor3,
+      appBar: header(),
+    );
+  }
+}
