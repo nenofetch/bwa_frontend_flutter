b50fd18673accfe151cc7d474e4401273cc7eff0
Neno Arisma <nenoarisma@gmail.com>
2023-05-20 21:25:04 +0700

    feat: adding chat page style

--- lib/pages/home/chat_page.dart
+++ lib/pages/home/chat_page.dart
@@ -4,15 +4,87 @@ import 'package:shamo_frontend/themes.dart';
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Chat Page',
-        style: primaryTextStyle.copyWith(
-          fontSize: 12,
+  Widget header() {
+    return AppBar(
+      backgroundColor: backgroundColor1,
+      title: Text('Message Support'),
+      centerTitle: true,
+      elevation: 0,
+      automaticallyImplyLeading: false,
+    );
+  }
+
+  Widget content() {
+    return Expanded(
+      child: Container(
+        width: double.infinity,
+        color: backgroundColor3,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/icon_headset.png',
+              width: 80,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Opss no message yet?',
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: medium,
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Text(
+              'You have never done a transaction',
+              style: subtitleTextStyle.copyWith(
+                fontSize: 14,
+                fontWeight: reguler,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 44,
+              child: TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 10,
+                  ),
+                  backgroundColor: primaryColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                child: Text(
+                  'Explore Store',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        header(),
+        content(),
+      ],
     );
   }
 }
