b1f6064f5aabd8f9e206899dca622c54c56795e6
Neno Arisma <nenoarisma@gmail.com>
2023-06-16 23:26:35 +0700

    feat: adding auth service

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:shamo_frontend/models/user_model.dart';
+
+class AuthService {
+  String baseUrl = 'myapi.justnear.my.id';
+  Future<UserModel> register({
+    String name,
+    String username,
+    String email,
+    String password,
+  }) async {
+    var url = Uri.https(baseUrl, '/api/register');
+    var headers = {'Content-Type': 'application/json'};
+    var body = jsonEncode({
+      'name': name,
+      'username': username,
+      'email': email,
+      'password': password
+    });
+
+    var response = await http.post(url, headers: headers, body: body);
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body)['data'];
+      UserModel user = UserModel.fromJson(data['user']);
+      user.token = 'Bearer ' + data['access_token'];
+
+      return user;
+    } else {
+      throw Exception('Failed Authentication');
+    }
+  }
+}
