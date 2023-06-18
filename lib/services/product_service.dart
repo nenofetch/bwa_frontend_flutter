e51eeb80377e6d6919a3c593389a51729d40a060
Neno Arisma <nenoarisma@gmail.com>
2023-06-18 22:04:44 +0700

    feat: adding product service

---
+++ lib/services/product_service.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:shamo_frontend/models/product_model.dart';
+
+class ProductService {
+  String baseUrl = 'myapi.justnear.my.id';
+
+  Future<List<ProductModel>> getProducts() async {
+    var url = Uri.https(baseUrl, '/products');
+    var headers = {'Content-Type': 'application/json'};
+
+    var response = await http.get(url, headers: headers);
+
+    if (response.statusCode == 200) {
+      List data = jsonDecode(response.body)['data']['data'];
+      List<ProductModel> products = [];
+      for (var item in data) {
+        products.add(ProductModel.fromJson(item));
+      }
+      return products;
+    } else {
+      throw Exception('Failed Get Products');
+    }
+  }
+}
