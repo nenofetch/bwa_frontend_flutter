edb152aa6ac1be4af091690eba0a37178c703483
Neno Arisma <nenoarisma@gmail.com>
2023-07-08 20:09:22 +0700

    chore: update profile page

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -24,7 +24,7 @@ class ProfilePage extends StatelessWidget {
               children: [
                 ClipOval(
                   child: Image.network(
-                    '${user.profilePhotoUrl}',
+                    user.profilePhotoUrl!,
                     width: 64,
                   ),
                 ),
